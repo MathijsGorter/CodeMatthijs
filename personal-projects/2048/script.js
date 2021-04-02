1ae6ab5cf811cbb69493d2744343dd14a250f707
Matthijs-Gorter <72670748+Matthijs-Gorter@users.noreply.github.com>
2021-04-02 20:32:14 +0200

    begin

---
+++ personal-projects/2048/script.js
@@ -0,0 +1,57 @@
+var grid = [[0]];
+
+makeNewGrid();
+
+document.onkeydown = checkKey;
+
+function makeNewGrid() {
+  let currentRow = [0];
+  grid.length = 0;
+  for (let colums = 1; colums < 5; colums++) {
+    currentRow.length = 0;
+    for (let row = 1; row < 5; row++) {
+      randomNumber = Math.floor(Math.random() * 100)
+      if (randomNumber < 30) {
+        currentRow.push (0)
+        // document.getElementById(row + "-" + colums).innerHTML = "";
+      } else if (randomNumber < 80) {
+        currentRow.push (2)
+        // document.getElementById(row + "-" + colums).innerHTML = "2";
+      } else {
+        currentRow.push (4)
+        // document.getElementById(row + "-" + colums).innerHTML = "4";
+      }
+    }
+    grid.push([])
+  }
+  updateUI();
+}
+
+function checkKey(e) {
+
+    e = e || window.event;
+sajdfhkp
+    if (e.keyCode == '38') {
+      console.log("up")
+    }
+    else if (e.keyCode == '40') {
+      console.log("down")
+    }
+    else if (e.keyCode == '37') {
+      console.log("left")
+
+    }
+    else if (e.keyCode == '39') {
+      console.log("right")
+
+    }
+
+}
+function updateUI() {
+  for (let colum = 1; colum < 5; colum++) {
+    for (let row = 1; row < 5; row++) {
+      document.getElementById(row + "-" + colum).innerHTML = grid[colum[row]];
+
+    }
+  }
+}
